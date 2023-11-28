f25b45f205023e2505e5c03207ab918e1fbd2620
jmckoy555 <jmckoy555@gmail.com>
2023-11-28 01:07:18 -0500

    updated portfolio information

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Creative", "Developer", "Podcaster", "Designer", "Hardworker","Innovator"],
+        strings: ["Creative", "Developer", "Designer", "Cloud Practioner","Innovator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Creative", "Developer", "Podcaster", "Designer", "Hardworker","Innovator"],
+        strings: ["Creative", "Developer", "Designer", "Cloud Practioner","Innovator"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -54,24 +54,3 @@ $(document).ready(function () {
 
         });
     });
-//     $('.carousel').owlCarousel({
-//         margin: 20,
-//         loop: true,
-//         autoplayTimeOut: 2000,
-//         autoplayHoverPause: true,
-//         responsive: {
-//             0: {
-//                 items: 1,
-//                 nav: false
-//             },
-//             600: {
-//                 items: 2,
-//                 nav: false
-//             },
-//             1000: {
-//                 items: 3,
-//                 nav: false
-//             }
-//         }
-//     });
-// });
